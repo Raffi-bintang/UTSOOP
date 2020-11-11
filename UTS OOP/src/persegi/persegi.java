41a0a0310781234a8f913f4ed677e601487e703a
Raffi-bintang <raffibintang5@gmail.com>
2020-11-11 12:31:54 +0700

    UTS OOP

---
+++ UTS OOP/src/persegi/persegi.java
@@ -0,0 +1,24 @@
+
+package persegi;
+
+
+public class persegi {
+
+   
+    public static void main(String[] args) {
+        int sisi;
+        int hasilkeliling;
+        int hasilluas;
+        
+        sisi = 24;
+        
+        hasilkeliling = sisi * 4;
+        hasilluas = sisi * sisi;
+        
+        System.out.println ("Hasil Keliling Persegi   : " + hasilkeliling);
+        System.out.println ("Hasil Luas Persegi  : " + hasilluas);
+        
+    }
+    
+    }
+   
